578051397112b3550c1334d729e1036979130fe1
isiagi <isiagigeofrey0@gmail.com>
2023-02-14 09:21:02 +0300

    add hero section

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,29 @@
+import "./hero.css"
+import heroImage from "../../assets/hero_isiagi_geofrey.jpg";
+import Button from "../button/Button";
+
+function Hero() {
+  return (
+    <div className="hero__container">
+      <div className="middle hero__wrapper">
+        <div className="hero__first">
+          <h1 className="hero__h1">I am A Developer Geofrey Isiagi</h1>
+          <p className="hero__para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus luctusnec ullamcorper mattis, pulvinar dapibus leop
+            llamcorper, lorem ipsum.
+          </p>
+          <Button name="Learn More"/>
+         
+        </div>
+        <div className="hero__second">
+          <img src={heroImage} alt="hero_isiagi_geofrey.jpg" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
+
+// *165*1*01*774002535*46112*age 35*pin23456#
