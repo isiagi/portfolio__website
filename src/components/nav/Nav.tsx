88cf8362141944936eab5b38bc9aad0cc865e778
isiagi <isiagigeofrey0@gmail.com>
2023-02-15 14:45:00 +0300

    makes nav responsive

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,23 +1,48 @@
+import { Link } from "react-router-dom";
 import Button from "../button/Button";
+import { CgDetailsMore } from "react-icons/cg";
+import { ImCancelCircle } from "react-icons/im";
 import "./nav.css";
+import { useState } from "react";
 
-type Props = {};
+function Nav() {
+  const [open, setOpen] = useState(false);
 
-function Nav({}: Props) {
   return (
     <nav className="nav__container">
       <div className="middle nav__wrapper">
         <div className="nav__header">
-          <h1>Isiagi Geofrey</h1>
+          <Link to="/">
+            <h1>Isiagi Geofrey</h1>
+          </Link>
         </div>
-        <div className="nav__ul">
+        <div className={`nav__ul ${open ? "active" : ""}`}>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Portfolio</li>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/">
+              <li>About</li>
+            </Link>
+            <Link to="/">
+              <li>Portfolio</li>
+            </Link>
           </ul>
         </div>
-        <Button name="Hire Me"/>
+        <Button name="Hire Me" />
+        <div className="nav__icons">
+          {open ? (
+            <ImCancelCircle
+              className="nav__icon"
+              onClick={() => setOpen(false)}
+            />
+          ) : (
+            <CgDetailsMore
+              className="nav__icon"
+              onClick={() => setOpen(true)}
+            />
+          )}
+        </div>
       </div>
     </nav>
   );
