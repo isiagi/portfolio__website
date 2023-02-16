0460d3ff4738554b31bd5e818561f85bb7b7ee49
isiagi <isiagigeofrey0@gmail.com>
2023-02-16 13:24:11 +0300

    adds mini header

---
+++ src/components/min/Min.tsx
@@ -0,0 +1,15 @@
+import "./min.css";
+
+type Props = {};
+
+function Min({}: Props) {
+  return (
+    <div>
+      <div className="mini">
+        <h2 className="mini__title">Contact</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Min;
