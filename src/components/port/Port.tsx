e501b31e4cbdc1cc93c0f8fc341b6eb55a9f4941
isiagi <isiagigeofrey0@gmail.com>
2023-03-08 17:44:45 +0300

    refactor: Create array for content

--- src/components/port/Port.tsx
+++ src/components/port/Port.tsx
@@ -1,4 +1,4 @@
-import "./port.css"
+import "./port.css";
 import img1 from "../../assets/port.png";
 import img2 from "../../assets/port1.png";
 import img3 from "../../assets/port2.png";
@@ -7,75 +7,78 @@ import Button from "../button/Button";
 
 type Props = {};
 
+const data = [
+  {
+    id: 1,
+    image: img1,
+    para: `This is a website made with Reactjs/NextJs and CSS. The purpose
+    of this website is to display the different services offer and
+    let the user be to email the company.`,
+    view: "View",
+    github: "GitHub",
+    viewLink: '',
+    githubLink: ''
+  },
+  {
+    id: 2,
+    image: img2,
+    para: `This website displays services offered by a shipping company. It
+    was developed with ReactJs and CSS. I was able improve on my CSS
+    and make something nice.`,
+    view: "View",
+    github: "GitHub",
+    viewLink: '',
+    githubLink: ''
+  },
+  {
+    id: 3,
+    image: img3,
+    para: ` This is a website serving information about HappiTravel. The
+    website was created with ReactJS and CSS with some more React
+    libraries.`,
+    view: "View",
+    github: "GitHub",
+    viewLink: '',
+    githubLink: ''
+  },
+  {
+    id: 4,
+    image: img4,
+    para: `This Website let's you view houses for sale and rent. The
+    project was create with React/NextJs, Chakra-UI and interacts
+    with Rapid Api to fetch the information.`,
+    view: "View",
+    github: "GitHub",
+  },
+];
+
 function Port({}: Props) {
   return (
     <div className="port__container">
       <div className="port__wrapper">
         <div className="port__grid">
-          <div className="port__item">
-            <div className="port__img">
-              <img src={img1} alt="" />
+          {data.map(({ id, image, para, view, github }) => (
+            <div key={id} className="port__item">
+              <div className="port__img">
+                <img src={image} alt="" />
+              </div>
+              <div className="port__details">
+                <p>{para}</p>
+              </div>
+              <div className="port__buttons">
+                <Button
+                  name={view}
+                  link="https://www.hitexconsortium.com/"
+                  path={false}
+                />
+                <Button
+                  name={github}
+                  link="https://github.com/isiagi/medical"
+                  path={false}
+                />
+              </div>
             </div>
-            <div className="port__details">
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
-                nihil iste aspernatur sit ab officia molestias excepturi iusto,
-                facilis tempora laboriosam debitis?
-              </p>
-            </div>
-            <div className="port__buttons">
-              <Button name="View" />
-              <Button name="GitHub" />
-            </div>
-          </div>
-          <div className="port__item">
-            <div className="port__img">
-              <img src={img2} alt="" />
-            </div>
-            <div className="port__details">
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
-                nihil iste aspernatur sit ab officia molestias excepturi iusto,
-                facilis tempora laboriosam debitis?
-              </p>
-            </div>
-            <div className="port__buttons">
-              <Button name="View" />
-              <Button name="GitHub" />
-            </div>
-          </div>
-          <div className="port__item">
-            <div className="port__img">
-              <img src={img3} alt="" />
-            </div>
-            <div className="port__details">
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
-                nihil iste aspernatur sit ab officia molestias excepturi iusto,
-                facilis tempora laboriosam debitis?
-              </p>
-            </div>
-            <div className="port__buttons">
-              <Button name="View" />
-              <Button name="GitHub" />
-            </div>
-          </div>
-          <div className="port__item">
-            <div className="port__img">
-              <img src={img4} alt="" />
-            </div>
-            <div className="port__details">
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
-                nihil iste aspernatur sit ab officia molestias excepturi iusto,
-                facilis tempora laboriosam debitis?
-              </p>
-            </div>
-            <div className="port__buttons">
-              <Button name="View" />
-              <Button name="GitHub" />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
